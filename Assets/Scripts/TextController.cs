5d0b9b903ca82bc89c1e301492e3ffeaf4d62e89
Fabutan <30437917+Fabutan@users.noreply.github.com>
2018-03-29 14:04:19 -0500

    Initial commit

---
+++ Assets/Scripts/TextController.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text content;
+
+	private enum States {entranceway0, entranceway1, lightswitch, lookaround, coat, leave, listen,smokeDetectorReset, investigateBeeping,endGame, investigateScratching, scratchingSuccess, scratchingFailure};
+	private States myState;
+
+	// Use this for initialization
+	void Start () {
+		myState = States.entranceway0;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (myState == States.entranceway0) 				{state_entranceway0 ();}
+		else if (myState == States.lightswitch) 			{state_lightswitch ();}
+		else if (myState == States.lookaround) 				{state_lookaround ();}
+		else if (myState == States.coat) 					{state_coat ();}
+		else if (myState == States.leave) 					{state_leave ();}
+		else if (myState == States.listen) 					{state_listen ();}
+		else if (myState == States.investigateBeeping) 		{state_investigateBeeping ();}
+		else if (myState == States.investigateScratching) 	{state_investigateScratching ();}
+		else if (myState == States.scratchingFailure) 		{state_scratchingFailure ();}
+		else if (myState == States.scratchingSuccess) 		{state_scratchingSuccess ();}
+		else if (myState == States.smokeDetectorReset) 		{state_smokeDetectorReset ();}
+		else if (myState == States.endGame) 				{state_endGame ();}
+			
+	}
+
+
+	void state_entranceway0(){
+		content.text = "Boots dripping from the rain, I hang my coat on the nearest hook " +
+		"adorning the entranceway. I know this place like the back of my hand, it's colorful " +
+		"turquoise lapping at the edges of a tan stained wooden floor.\n\n" +
+		"Press 1 to Turn On Lights \n\n" +
+		"Press 2 to Grab Your Coat \n\n" +
+		"Press 3 to Listen To Surroundings";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.lightswitch;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.coat;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.listen;
+		}
+	}
+
+	void state_entranceway1(){
+		content.text = "You stand blinking in an old Saint Boniface house. Though the task ahead /n/n" +
+			"weighs heavily on you, you steel yourself for action. \n\n" +
+			"Press 1 to Turn Look Around \n\n" +
+			"Press 2 to Grab Your Coat \n\n" +
+			"Press 3 to Listen To Surroundings";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.lookaround;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.coat;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.listen;
+		}
+	}
+
+	void state_lookaround(){
+		content.text = "The evidence is all around you, hair everywhere, that musky smell." + 
+			"If only you could close your eyes and convince yourself you were somewhere else.\n\n" +
+			"Press 2 to Grab Your Coat \n\n" +
+			"Press 3 to Listen To Surroundings";
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.coat;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.listen;
+		}
+	}
+
+
+
+	void state_lightswitch(){
+		content.text = "You grope the wall opposite your coat for a few brief moments before your " +
+		"thumb finds the familiar little click of the switch. \n\n " +
+		"Your eyes burn slightly as they adjust to your newly illuminated surroundings.\n\n" +
+		"Press 1 to Look Around \n\n" +
+		"Press 2 to Grab Your Coat \n\n" +
+		"Press 3 to Listen To Surroundings \n\n";
+		
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.lookaround;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.coat;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.listen;	
+		}
+	}
+
+	void state_coat(){
+		content.text = "You question why you even entered this old house. You Grab your coat, ready to leave. As uninviting as the weather " +
+		"may be, it's still easier to think about than what lies ahead in wait. \n\n" +
+		"Press 1 to Go Outside \n\n" +
+		"Press 2 to Return Coat \n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.leave;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.entranceway0;
+		}
+	}
+	void state_leave(){
+		content.text = "You pause for a brief moment unsure of of your decision, but It's too much to bear. " +
+				"You shake the excess water from your coat, throw it over your shoulders and vanish into " +
+				"the rainy night. \n\n" +
+				"The End\n\n" +
+				"Press Spacebar to Admit Defeat.";
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				myState = States.endGame;
+		} 
+	}
+
+	void state_listen (){
+		content.text = "A faint beeping can be heard coming from somewhere above you.\n\n" +
+			"Something small is scratching at wood nearby.\n\n" +
+			"Press 1 to Investigate The Beeping \n\n" +
+			"Press 2 to Investigate the Scratching\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.investigateBeeping;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.investigateScratching;
+		}
+	
+	}
+
+	void state_investigateBeeping (){
+		content.text = "You slowly walk through the house, ears trained on the faint intermittent beeps " +
+			"that seem to be coming form the second floor.\n\n" +
+			"It's a smoke detector, the battery must be dead. Resetting the device ceases the beeping.\n\n" +
+			"Now the scratching has stopped.\n\n" +
+			"Press 1 to Investigate the Scratching\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.smokeDetectorReset;
+		}
+
+
+	}
+
+	void state_investigateScratching (){
+		content.text = "As you approach a closed door on the second foor, you notice the scratching getting more and more frantic.\n\n" +
+			"You reach for the handle, before pausing to think.\n\n" +
+			"Press 1 to Open The Door\n\n" +
+			"Press 2 to Listen Closely\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.scratchingFailure;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.listen;
+		}
+
+	}
+
+
+	void state_scratchingFailure (){
+		content.text = "Turning the doorknob you feel a sudden burst as the door flies open.\n\n" +
+			"OH GOD, OH NO! A brown furball skitters past you, leaving nasty streaks of poop everywhere she goes, what a mess!\n\n" +
+			"This is the worst thing ever.\n\n" +
+			"Press 1 to Clean It Up";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.endGame;
+		} 
+
+	}
+
+	void state_smokeDetectorReset (){
+		content.text = "With the beeping stopped, you hear the scratching slow to a halt, then silence.\n\n" +
+			"You follow the scratching to a bedroom door, and slowly reach for the doorknob.\n\n" +
+			"Press 1 To Open Door";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.scratchingSuccess;
+		} 
+
+	}
+
+	void state_scratchingSuccess (){
+		content.text = "Turning the doorknob you see a little dog sitting patiently for you. Beppu has waited patiently for a walk.\n\n" +
+		"Press Spacebar To Take Her For a Walk\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myState = States.endGame;
+		} 
+
+	}
+
+	void state_endGame (){
+		content.text = "The End\n\n" +
+			"Press Space To Retry";
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			state_entranceway0 ();
+		}
+	}
+}
---
+++ Assets/Scripts/TheChase.cs
@@ -0,0 +1,283 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TheChase : MonoBehaviour {
+
+	public Text content;
+
+	private enum States {
+		state1Escape,
+		state2Steak,
+		state3Leash,
+		state4Outside0,
+		state5Outside1,
+		state6Chase0,
+		state6Chase1,
+		state7UseSteak,
+		state8Approach,
+		state9Whistle,
+		state10GoBack,
+		state11WasteSteak,
+		state12Pounce,
+		state13OpenGate,
+		state14Herd,
+		state15Command,
+		state16SteakYard,
+		state17GoodGirl,
+		state18BadGirl
+	};
+	private States myState;
+
+	// Use this for initialization
+	void Start () {
+		myState = States.state1Escape;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (myState == States.state1Escape) 				{Escape ();}
+		else if (myState == States.state2Steak) 			{Steak ();}
+		else if (myState == States.state3Leash) 			{Leash ();}
+		else if (myState == States.state4Outside0) 			{Outside0 ();}
+		else if (myState == States.state5Outside1) 			{Outside1 ();}
+		else if (myState == States.state6Chase0) 			{Chase0 ();}
+		else if (myState == States.state6Chase1) 			{Chase1 ();}
+		else if (myState == States.state7UseSteak) 			{UseSteak ();}
+		else if (myState == States.state8Approach) 			{Approach ();}
+		else if (myState == States.state9Whistle) 			{Whistle ();}
+		else if (myState == States.state10GoBack)	 		{GoBack ();}
+		else if (myState == States.state11WasteSteak) 		{WasteSteak ();}
+		else if (myState == States.state12Pounce) 			{Pounce ();}
+		else if (myState == States.state13OpenGate) 		{OpenGate ();}
+		else if (myState == States.state14Herd)	 			{Herd ();}
+		else if (myState == States.state15Command) 			{Command ();}
+		else if (myState == States.state16SteakYard) 		{SteakYard ();}
+		else if (myState == States.state17GoodGirl) 		{GoodGirl ();}
+		else if (myState == States.state18BadGirl) 			{BadGirl ();}
+	}
+
+	#region State Handler Methods
+	void Escape(){
+		content.text = "Shit! The door was only open a crack, and just like that, Beppu was on the loose!\n\n" +
+		"Press 1 to Go After Her!\n\n" +
+		"Press 2 to Grab A Leash \n\n" +
+		"Press 3 to Grab A Snack";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state4Outside0;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state3Leash;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.state2Steak;
+		}
+	}
+
+	void Steak(){
+		content.text = "You grabbed a nice bloody raw steak from the fridge, but now what!?\n\n" +
+			"Press 1 to Go After Her!\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state5Outside1;
+		}
+	}
+
+	void Leash(){
+		content.text = "Where the hell is her leash! It's nowhere to be found, you don't have " +
+			"time for this she's getting away!\n\n" +
+			"Press 1 to Go After Her!\n\n" +
+			"Press 2 to Grab A Snack";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state4Outside0;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state2Steak;
+		} 
+	}
+
+	void Outside0(){
+		content.text = "She's bolting back an forth between traffic! But you can't get her attention!" +
+			"Maybe a distraction would work?\n\n" +
+			"Press 1 to Give Chase!\n\n" +
+			"Press 2 to Go Back";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state6Chase0;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state1Escape;
+		} 
+	}
+
+	void Chase0(){
+		content.text = "She's like a rabbit, you would never be able to catch her like this!\n\n" +
+		"Press 1 to Continue\n\n";
+
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				myState = States.state4Outside0;
+			}
+		}
+
+	void Outside1(){
+		content.text = "She's Bolting back and forth between traffic but you can't get her attention!" +
+		"Maybe a distraction would work?\n\n" +
+		"Press 1 to Give Chase!\n\n" +
+		"Press 2 to Use Steak\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state6Chase1;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state7UseSteak;
+		} 
+	}
+
+	void Chase1(){
+		content.text = "She's like a rabbit, you would never be able to catch her like this!\n\n" +
+			"Press 1 to Continue\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state5Outside1;
+		}
+	}
+
+	void UseSteak(){
+		content.text = "The Raw Meat catches her eye, she immediately stops and stares you down.\n\n" +
+			"Press 1 to Approach Slowly!\n\n" +
+			"Press 2 to Whistle To Her\n\n" +
+			"Press 3 to Go Back";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state8Approach;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state9Whistle;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.state10GoBack;
+		}
+	}
+
+	void Approach(){
+		content.text = "It's clear that she's not going to fall for that, and she's off like a bullet!.\n\n" +
+		"Press 1 to Try Something Else\n\n";
+
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				myState = States.state5Outside1;
+			}
+	}
+
+	void Whistle(){
+		content.text = "Phweet*! Nothing, she's too interested in the neighbor's compost bin to notice you.\n\n" +
+		"Press 1 to Try Something Else\n\n";
+
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				myState = States.state5Outside1;
+			}
+	}
+
+
+	void GoBack(){
+		content.text = "It's working, she's cautiously following you back towards the yard.\n\n" +
+			"Press 1 to Open The Gate\n\n" +
+			"Press 2 to Toss Some Steak\n\n" +
+			"Press 3 to Pounce on Her!\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state13OpenGate;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state11WasteSteak;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.state12Pounce;
+		}
+	}
+
+
+	void WasteSteak(){
+		content.text = "She snaps it out of the air with glee! More Please!\n\n" +
+			"Press 1 to Try Something Else\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state10GoBack;
+		}
+	}
+
+	void Pounce(){
+		content.text = "Not only does she squirm out of your grip, but you've lost the tenuous trust " +
+			"that you worked so hard to establish.\n\n" +
+			"Press 1 to Try Something Else\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state5Outside1;
+		}
+	}
+
+	void OpenGate(){
+		content.text = "The gate is now open, but she's still not totally convinced.\n\n" +
+			"Press 1 to Herd Her Into The Yard\n\n" +
+			"Press 2 to Command Her To Come\n\n" +
+			"Press 3 to Use Steak";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state14Herd;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state15Command;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			myState = States.state16SteakYard;
+		}
+	}
+
+
+	void Herd(){
+		content.text = "You circle around her slowly but she's more agile than you!" +
+			"She immediately bolts.\n\n" +
+			"Press 1 to Try Something Else\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state5Outside1;
+		}
+	}
+
+	void Command(){
+		content.text = "BEPPU COME HERE!!!\n\n\n\n Even your mightiest commands fall on deaf little ears.\n\n" +
+			"Press 1 to Try Something Else\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state13OpenGate;
+		}
+	}
+
+	void SteakYard(){
+		content.text = "You toss a delectible morsel into the back yard.\n" +
+			"She fell for it! Slamming the gate behind her, your fuzzy little prisoner " + 
+			"is once again where she belongs. \n\n" +
+			"Press 1 to Good Girl \n\n" +
+			"Press 2 to BAD GIRL! \n\n";
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			myState = States.state17GoodGirl;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			myState = States.state18BadGirl;
+		}
+	}
+
+	void GoodGirl(){
+		content.text = "She stares blankly into the distance, it would seem your praise went unnoticed.\n" +
+			"You win, I guess... The End\n\n" +
+			"Press Spacebar To Play Again\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myState = States.state1Escape;
+		} 
+	}
+
+	void BadGirl(){
+		content.text = "She stares blankly into the distance, it would seem your punishment went unnoticed.\n" +
+			"You win, I guess... The End\n\n" +
+			"Press Spacebar To Play Again\n\n";
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myState = States.state1Escape;
+		} 
+	}
+	#endregion
+}
